3a8ba72b11136d8ca53bb6f37fea7d2d40877f78
Prathamesh S. Dahake <pratham.dahakepsd@gmail.com>
2026-06-11 16:24:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 function stringChop(str, size) {
+	if(!str && str.length === 0) return [];
+	 if (!size) return [str];
   // your code here
   let chunkArr = [];
   let chunk = "";
 
   for (let i = 0; i < str.length; i++) {
     chunk += str[i];
-    if (chunk.length === Number(size)) {
+    if (chunk.length === parseInt(size)) {
       chunkArr.push(chunk);
       chunk = "";
     }
   }
-  
+    
   if (chunk.length > 0) {
     chunkArr.push(chunk);
   }
@@ -18,7 +20,7 @@ function stringChop(str, size) {
 } 
 
 // Do not change the code below
-const str = prompt("Enter String.");
+const str = prompt("Enter String."); 
 const size = prompt("Enter Chunk Size.");
 alert(stringChop(str, size));
 
